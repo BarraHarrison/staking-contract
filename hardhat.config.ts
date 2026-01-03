f385d65715c3dfe404ed292df9a4c6460460e1d9
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2026-01-03 17:55:49 +0900

    Hardhat config file is compatible with the JS deployment script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,22 +1,35 @@
 import "@nomicfoundation/hardhat-ethers";
 import "@nomicfoundation/hardhat-toolbox-mocha-ethers";
+
 import { defineConfig, configVariable } from "hardhat/config";
 
 export default defineConfig({
   solidity: {
     profiles: {
-      default: { version: "0.8.28" },
+      default: {
+        version: "0.8.28",
+      },
       production: {
         version: "0.8.28",
         settings: {
-          optimizer: { enabled: true, runs: 200 },
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
         },
       },
     },
   },
+
   networks: {
-    hardhatMainnet: { type: "edr-simulated", chainType: "l1" },
-    hardhatOp: { type: "edr-simulated", chainType: "op" },
+    hardhatMainnet: {
+      type: "edr-simulated",
+      chainType: "l1",
+    },
+    hardhatOp: {
+      type: "edr-simulated",
+      chainType: "op",
+    },
     sepolia: {
       type: "http",
       chainType: "l1",
