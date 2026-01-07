ce3baca5d2d53e13cf40f4f36a64ce8ee39c9796
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2026-01-07 10:34:45 +0900

    ERC20 Staking test to see if the State Token and Reward Token is working correctly

---
+++ test/Staking.test.js
@@ -0,0 +1,56 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Staking contract", function () {
+    let stakeToken;
+    let rewardToken;
+    let staking;
+    let owner;
+    let user;
+
+    const INITIAL_SUPPLY = ethers.utils.parseEther("1000000");
+    const STAKE_AMOUNT = ethers.utils.parseEther("100");
+    const REWARD_RATE = ethers.utils.parseEther("1");
+    const REWARD_FUND = ethers.utils.parseEther("100000");
+
+    beforeEach(async function () {
+        [owner, user] = await ethers.getSigners();
+
+        const StakeToken = await ethers.getContractFactory("StakeToken");
+        stakeToken = await StakeToken.deploy(INITIAL_SUPPLY);
+        await stakeToken.deployed();
+
+        const RewardToken = await ethers.getContractFactory("RewardToken");
+        rewardToken = await RewardToken.deploy();
+        await rewardToken.deployed();
+
+        const Staking = await ethers.getContractFactory("Staking");
+        staking = await Staking.deploy(
+            stakeToken.address,
+            rewardToken.address,
+            REWARD_RATE
+        );
+        await staking.deployed();
+        await rewardToken.mint(staking.address, REWARD_FUND);
+        await stakeToken.transfer(user.address, STAKE_AMOUNT);
+    });
+
+    it("allows a user to stake and earn rewards over time", async function () {
+
+        await stakeToken.connect(user).approve(staking.address, STAKE_AMOUNT);
+        await staking.connect(user).stake(STAKE_AMOUNT);
+        await ethers.provider.send("evm_increaseTime", [10]);
+        await ethers.provider.send("evm_mine");
+
+        const earned = await staking.earned(user.address);
+        expect(earned).to.be.gt(0);
+
+        await staking.connect(user).claimReward();
+
+        const rewardBalance = await rewardToken.balanceOf(user.address);
+        expect(rewardBalance).to.be.gt(0);
+
+        const remainingRewards = await staking.earned(user.address);
+        expect(remainingRewards).to.equal(0);
+    });
+});
