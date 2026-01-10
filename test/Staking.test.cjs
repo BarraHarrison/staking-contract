a6d5a3ae33295497d9938f17fd4ff05bc9ab7935
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2026-01-10 16:14:38 +0900

    Using isZero to see if there is no conflict errors

--- test/Staking.test.cjs
+++ test/Staking.test.cjs
@@ -51,6 +51,6 @@ describe("Staking contract", function () {
         expect(rewardBalance.gt(0)).to.equal(true);
 
         const remainingRewards = await staking.earned(user.address);
-        expect(remainingRewards).to.equal(ethers.constants.Zero);
+        expect(remainingRewards.isZero()).to.equal(true);
     });
 });
